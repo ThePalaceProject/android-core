25ecb5b21130c7fda2db52ecf8959cc1bf0fbc64
Ray Lee <ray.lee@lyrasis.org>
2021-03-31 20:17:01 -0400

    Adapt CI for Lyrasis.

--- .ci-local/credentials.sh
+++ .ci-local/credentials.sh
@@ -15,14 +15,14 @@ info()
   echo "credentials-local.sh: info: $1" 1>&2
 }
 
-if [ -z "${NYPL_NEXUS_USER}" ]
-then
-  fatal "NYPL_NEXUS_USER is not defined"
-fi
-if [ -z "${NYPL_NEXUS_PASSWORD}" ]
-then
-  fatal "NYPL_NEXUS_PASSWORD is not defined"
-fi
+#if [ -z "${NYPL_NEXUS_USER}" ]
+#then
+#  fatal "NYPL_NEXUS_USER is not defined"
+#fi
+#if [ -z "${NYPL_NEXUS_PASSWORD}" ]
+#then
+#  fatal "NYPL_NEXUS_PASSWORD is not defined"
+#fi
 
 #------------------------------------------------------------------------
 # Copy credentials into place.
@@ -30,7 +30,7 @@ fi
 
 info "installing keystore"
 
-cp -v ".ci/credentials/APK Signing/nypl-keystore.jks" \
+cp -v ".ci/credentials/APK Signing/keystore.jks" \
   "release.jks" || exit 1
 
 #------------------------------------------------------------------------
@@ -40,31 +40,31 @@ cp -v ".ci/credentials/APK Signing/nypl-keystore.jks" \
 mkdir -p "${HOME}/.gradle" ||
   fatal "could not create ${HOME}/.gradle"
 
-cat ".ci/credentials/APK Signing/nypl-keystore.properties" >> "${HOME}/.gradle/gradle.properties" ||
+cat ".ci/credentials/APK Signing/keystore.properties" >> "${HOME}/.gradle/gradle.properties" ||
   fatal "could not read keystore properties"
 
-CREDENTIALS_PATH=$(realpath ".ci/credentials") ||
-  fatal "could not resolve credentials path"
-
-SIMPLYE_CREDENTIALS="${CREDENTIALS_PATH}/SimplyE/Android"
-OPENEBOOKS_CREDENTIALS="${CREDENTIALS_PATH}/OpenEbooks/Android"
-
-if [ ! -d "${SIMPLYE_CREDENTIALS}" ]
-then
-  fatal "${SIMPLYE_CREDENTIALS} does not exist, or is not a directory"
-fi
-if [ ! -d "${OPENEBOOKS_CREDENTIALS}" ]
-then
-  fatal "${OPENEBOOKS_CREDENTIALS} does not exist, or is not a directory"
-fi
-
-cat >> "${HOME}/.gradle/gradle.properties" <<EOF
-org.librarysimplified.drm.enabled=true
-
-org.librarysimplified.nexus.depend=true
-org.librarysimplified.nexus.username=${NYPL_NEXUS_USER}
-org.librarysimplified.nexus.password=${NYPL_NEXUS_PASSWORD}
-
-org.librarysimplified.app.assets.openebooks=${OPENEBOOKS_CREDENTIALS}
-org.librarysimplified.app.assets.simplye=${SIMPLYE_CREDENTIALS}
-EOF
+#CREDENTIALS_PATH=$(realpath ".ci/credentials") ||
+#  fatal "could not resolve credentials path"
+#
+#SIMPLYE_CREDENTIALS="${CREDENTIALS_PATH}/SimplyE/Android"
+#OPENEBOOKS_CREDENTIALS="${CREDENTIALS_PATH}/OpenEbooks/Android"
+#
+#if [ ! -d "${SIMPLYE_CREDENTIALS}" ]
+#then
+#  fatal "${SIMPLYE_CREDENTIALS} does not exist, or is not a directory"
+#fi
+#if [ ! -d "${OPENEBOOKS_CREDENTIALS}" ]
+#then
+#  fatal "${OPENEBOOKS_CREDENTIALS} does not exist, or is not a directory"
+#fi
+#
+#cat >> "${HOME}/.gradle/gradle.properties" <<EOF
+#org.librarysimplified.drm.enabled=true
+#
+#org.librarysimplified.nexus.depend=true
+#org.librarysimplified.nexus.username=${NYPL_NEXUS_USER}
+#org.librarysimplified.nexus.password=${NYPL_NEXUS_PASSWORD}
+#
+#org.librarysimplified.app.assets.openebooks=${OPENEBOOKS_CREDENTIALS}
+#org.librarysimplified.app.assets.simplye=${SIMPLYE_CREDENTIALS}
+#EOF
