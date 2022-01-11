12cf49058ee145b7a007737c51edae5edf1c5b63
Ray Lee <ray.lee@lyrasis.org>
2022-01-11 17:54:17 -0500

    Don't show EULA screen on startup.

--- simplified-ui-splash/src/main/java/org/nypl/simplified/ui/splash/SplashFragment.kt
+++ simplified-ui-splash/src/main/java/org/nypl/simplified/ui/splash/SplashFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentResultListener
 import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
-import org.librarysimplified.documents.DocumentStoreType
-import org.librarysimplified.services.api.Services
 import org.nypl.simplified.listeners.api.FragmentListenerType
 import org.nypl.simplified.listeners.api.fragmentListeners
 import org.slf4j.LoggerFactory
@@ -29,36 +27,12 @@ class SplashFragment : Fragment(R.layout.splash_fragment), FragmentResultListene
   override fun onFragmentResult(requestKey: String, result: Bundle) {
     when (childFragmentManager.fragments.last()) {
       is BootFragment -> onBootCompleted()
-      is EulaFragment -> onEulaFinished()
       is MigrationProgressFragment -> onMigrationCompleted()
       is MigrationReportFragment -> onMigrationReportFinished()
     }
   }
 
   private fun onBootCompleted() {
-    val eula =
-      Services.serviceDirectory()
-        .requireService(DocumentStoreType::class.java)
-        .eula
-
-    if (eula != null && !eula.hasAgreed) {
-      showEula()
-    } else {
-      onEulaFinished()
-    }
-  }
-
-  private fun onEulaFinished() {
-    val eula =
-      Services.serviceDirectory()
-        .requireService(DocumentStoreType::class.java)
-        .eula
-
-    if (eula != null && !eula.hasAgreed) {
-      this.logger.error("eula declined: terminating")
-      requireActivity().finish()
-    }
-
     val migrationViewModel =
       ViewModelProvider(this)
         .get(MigrationViewModel::class.java)
