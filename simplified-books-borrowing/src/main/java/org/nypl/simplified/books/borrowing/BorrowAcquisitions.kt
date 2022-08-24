ea0228cf6c2b63c575ada20b961000799ba4e24b
nunommts <79104027+nunommts@users.noreply.github.com>
2022-08-24 17:08:48 +0100

    Fixed sample preview being handled as full content acquisition link (#136)
    * Fixed sample preview being handled as full content acquisition link
    
    * Moved sample/full-content handling logic
    
    * Removed unwanted condition and created unit test
    
    * Updated unit test
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Updated unit tests
    
    * no-op
    
    * Deleted unused method and imports

--- simplified-books-borrowing/src/main/java/org/nypl/simplified/books/borrowing/BorrowAcquisitions.kt
+++ simplified-books-borrowing/src/main/java/org/nypl/simplified/books/borrowing/BorrowAcquisitions.kt
@@ -1,6 +1,7 @@
 package org.nypl.simplified.books.borrowing
 
 import org.nypl.simplified.books.formats.api.BookFormatSupportType
+import org.nypl.simplified.opds.core.OPDSAcquisition
 import org.nypl.simplified.opds.core.OPDSAcquisitionFeedEntry
 import org.nypl.simplified.opds.core.OPDSAcquisitionPath
 import org.nypl.simplified.opds.core.OPDSAcquisitionPaths
@@ -20,7 +21,9 @@ object BorrowAcquisitions {
     entry: OPDSAcquisitionFeedEntry
   ): OPDSAcquisitionPath? {
     val paths = OPDSAcquisitionPaths.linearize(entry)
-    val filtered = paths.filter { support.isSupportedPath(it.asMIMETypes()) }
-    return filtered.firstOrNull() ?: return null
+    return paths
+      .filter { support.isSupportedPath(it.asMIMETypes()) }
+      // when borrowing a book, we need to ignore if the acquisition is for a sample/preview
+      .firstOrNull { it.source.relation != OPDSAcquisition.Relation.ACQUISITION_SAMPLE }
   }
 }
