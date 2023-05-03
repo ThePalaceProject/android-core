e33f89f2eb33380aa70ce9cba65076530cdee1d3
Jonah Dempcy <jdempcy@gmail.com>
2023-05-03 14:55:01 -0600

    Updated DeepLinksControllerType with correct DeepLinkEvent and removed unused imports

--- simplified-main/src/main/java/org/nypl/simplified/main/DeepLinksControllerType.kt
+++ simplified-main/src/main/java/org/nypl/simplified/main/DeepLinksControllerType.kt
@@ -1,29 +1,7 @@
 package org.nypl.simplified.profiles.controller.api
 
-import com.google.common.collect.ImmutableList
-import com.google.common.util.concurrent.FluentFuture
 import io.reactivex.Observable
-import org.nypl.simplified.accounts.api.AccountEvent
-import org.nypl.simplified.accounts.api.AccountID
-import org.nypl.simplified.accounts.api.AccountProviderType
-import org.nypl.simplified.accounts.database.api.AccountType
-import org.nypl.simplified.accounts.database.api.AccountsDatabaseNonexistentException
-import org.nypl.simplified.books.api.BookID
-import org.nypl.simplified.feeds.api.Feed
-import org.nypl.simplified.profiles.api.ProfileCreationEvent
-import org.nypl.simplified.profiles.api.ProfileDeletionEvent
-import org.nypl.simplified.profiles.api.ProfileDescription
-import org.nypl.simplified.profiles.api.ProfileEvent
-import org.nypl.simplified.profiles.api.ProfileID
-import org.nypl.simplified.profiles.api.ProfileNoneCurrentException
-import org.nypl.simplified.profiles.api.ProfileNonexistentAccountProviderException
-import org.nypl.simplified.profiles.api.ProfileReadableType
-import org.nypl.simplified.profiles.api.ProfileUpdated
-import org.nypl.simplified.profiles.api.ProfilesDatabaseType
-import org.nypl.simplified.profiles.api.idle_timer.ProfileIdleTimerType
-import org.nypl.simplified.taskrecorder.api.TaskResult
-import java.net.URI
-import java.util.SortedMap
+import org.nypl.simplified.main.DeepLinkEvent
 
 /**
  * The deep links controller.
@@ -35,6 +13,6 @@ interface DeepLinksControllerType {
    * @return An observable that publishes profile events
    */
 
-  fun deepLinkEvents(): Observable<ProfileEvent>
+  fun deepLinkEvents(): Observable<DeepLinkEvent>
 
 }
