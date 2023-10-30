6b513aa16fa447fdabaf7e2c72345f35b9288945
Nuno Matos <nuno.matos@toptal.com>
2023-10-30 22:42:46 +0000

    Added missing import

--- simplified-viewer-pdf-pdfjs/build.gradle.kts
+++ simplified-viewer-pdf-pdfjs/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation(libs.androidx.coordinatorlayout)
     implementation(libs.androidx.core)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.customview)
     implementation(libs.androidx.customview)
     implementation(libs.androidx.customview.poolingcontainer)
