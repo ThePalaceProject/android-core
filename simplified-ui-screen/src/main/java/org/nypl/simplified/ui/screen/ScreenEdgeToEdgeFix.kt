e635c2570171f8191cf2b19e4b5ac4cda6c601eb
Mark Raynsford <code@io7m.com>
2025-07-08 13:24:31 +0100

    Set nonexistent status bar to the correct color for day/night mode.

--- simplified-ui-screen/src/main/java/org/nypl/simplified/ui/screen/ScreenEdgeToEdgeFix.kt
+++ simplified-ui-screen/src/main/java/org/nypl/simplified/ui/screen/ScreenEdgeToEdgeFix.kt
@@ -1,5 +1,7 @@
 package org.nypl.simplified.ui.screen
 
+import android.app.Activity
+import android.content.res.Configuration
 import android.os.Build
 import android.view.View
 import androidx.core.view.ViewCompat
@@ -31,6 +33,33 @@ object ScreenEdgeToEdgeFix {
         val padBottom = systemBars.bottom
         view.setPadding(padLeft, padTop, padRight, padBottom)
         insets
+      }
+
+      /*
+       * Google want to pretend that they don't have a status bar, whilst also clearly having
+       * status bar icons. To facilitate this delusion, we have to set the status bar (that we
+       * don't have) to light or dark depending on the day/night mode. This is necessary so that
+       * we don't end up with black icons on a black background, and so on.
+       */
+
+      val configuration =
+        rootView.resources.configuration
+      val context =
+        rootView.context
+
+      if (context is Activity) {
+        val isNightMode =
+          (configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+        val decor =
+          context.window.decorView
+
+        if (isNightMode) {
+          decor.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        } else {
+          decor.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
       }
     }
   }
