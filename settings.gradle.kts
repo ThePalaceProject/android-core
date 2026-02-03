1ef23c9d945f9d04abe5419a68d8a0ee41442394
Mark Raynsford <code@io7m.com>
2026-02-03 11:35:28 +0000

    Use new Central snapshots URL.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -150,17 +150,11 @@ dependencyResolutionManagement {
         google()
 
         /*
-         * Allow access to the Sonatype snapshots repositories.
+         * Allow access to the Sonatype snapshots repository.
          */
 
         maven {
             url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-        }
-        maven {
-            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-        }
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
         }
 
         /*
